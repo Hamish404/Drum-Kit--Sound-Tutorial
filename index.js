128615636559f84c72701bdfcc07d9e4e6765c29
Hamish Tanner <hamishtanner@hotmail.com>
2024-11-14 17:39:33 +1000

    Change approach to how event listeners were attached

--- index.js
+++ index.js
@@ -2,50 +2,16 @@ const allButtons = document.querySelectorAll('.drum');
 
 allButtons.forEach(button => {
   button.addEventListener('click', (e) => {
-
-    switch (e.target.innerHTML) {
-
-      case 'w':
-        const audio1 = new Audio('sounds/crash.mp3');
-        audio1.play(); 
-      break;
-
-      case 'a':
-        const audio2 = new Audio('sounds/kick-bass.mp3');
-        audio2.play();
-      break;
-
-      case 's':
-        const audio3 = new Audio('sounds/snare.mp3');
-        audio3.play();
-      break;
-
-      case 'd':
-        const audio4 = new Audio('sounds/tom-1.mp3');
-        audio4.play();
-      break;
-
-      case 'j':
-        const audio5 = new Audio('sounds/tom-2.mp3');
-        audio5.play();
-      break;
-
-      case 'k':
-        const audio6 = new Audio('sounds/tom-3.mp3');
-        audio6.play();
-      break;
-
-      case 'l':
-        const audio7 = new Audio('sounds/tom-4.mp3');
-        audio7.play();
-      break;
-    }
+    makeSound(e.target.innerHTML);
   })
 })
 
 document.addEventListener('keydown', (e) => {
+  makeSound(e.key);
+})
 
-  switch (e.key) {
+const makeSound = (key) => {
+  switch (key) {
     
     case 'w':
       const audio1 = new Audio('sounds/crash.mp3');
@@ -82,8 +48,5 @@ document.addEventListener('keydown', (e) => {
       audio7.play(); 
     break;
   }
- 
-})
-
-
+}
 
